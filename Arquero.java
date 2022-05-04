d3e77bac87b61393b28c1a9c93d9901d0e7a5079
Packini <85806077+Packini@users.noreply.github.com>
2022-05-03 19:35:16 -0600

    Create Arquero.java

---
+++ Arquero.java
@@ -0,0 +1,7 @@
+package RPG;
+public class Arquero extends Player {
+	public Arquero (String clase, double Hp, double Mp, double ataque, double defensa) {
+		super(clase, Hp , Mp, ataque, defensa);
+	}
+	
+}
