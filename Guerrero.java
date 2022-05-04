be5a194a5d183a6818226653388df822833cfc7b
Packini <85806077+Packini@users.noreply.github.com>
2022-05-03 19:34:38 -0600

    Create Guerrero.java

---
+++ Guerrero.java
@@ -0,0 +1,8 @@
+package RPG;
+
+public class Guerrero extends Player{
+	public Guerrero (String clase, double Hp, double Mp, double ataque, double defensa) {
+		super(clase, Hp, Mp, ataque, defensa);
+	}
+	
+}
