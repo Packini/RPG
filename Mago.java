71adf3932f4cde228b4de3140edc5a23a9e305e4
Packini <85806077+Packini@users.noreply.github.com>
2022-05-03 19:35:00 -0600

    Create Mago.java

---
+++ Mago.java
@@ -0,0 +1,7 @@
+package RPG;
+public class Mago extends Player{
+	public Mago (String clase, double Hp, double Mp, double ataque, double defensa) {
+		super(clase, Hp, Mp, ataque, defensa);
+	}
+	
+}
